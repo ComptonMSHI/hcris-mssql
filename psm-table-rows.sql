805883fa6211f0fb7a988095f8feccd4b247a007
D.C. Compton <code@dccompton.com>
2018-06-30 16:14:09 -0500

    Adjust query joins

--- psm-table-rows.sql
+++ psm-table-rows.sql
@@ -9,6 +9,7 @@ CREATE PROC
 
 DROP TABLE IF EXISTS mcrFormData;
 
+
 SELECT
 	r.IMPORT_DT
 	, r.FORM
@@ -27,28 +28,39 @@ SELECT
 
     , CASE 
         WHEN cw.LINE_NUM Is Not Null 
-            THEN SUBSTRING(cw.LINE_NUM,1,3) ELSE w.LINE_NUM
+            THEN cw.LINE_NUM ELSE w.LINE_NUM
         END AS [LINE_NUM]
+        
+    , CASE 
+        WHEN cw.SUBLINE_NUM Is Not Null 
+            THEN cw.SUBLINE_NUM ELSE w.SUBLINE_NUM
+        END AS [SUBLINE_NUM]        
 
-	, w.LINE_DESC	
+    , w.LINE_DESC	
 
-    , CASE
-        WHEN cw.CLMN_NUM Is Not Null
-            THEN SUBSTRING(cw.CLMN_NUM,1,3) ELSE w.CLMN_NUM
+    , CASE 
+        WHEN cw.CLMN_NUM Is Not Null 
+            THEN cw.CLMN_NUM ELSE w.CLMN_NUM
         END AS [CLMN_NUM]
+        
+    , CASE 
+        WHEN cw.SUBCLMN_NUM Is Not Null 
+            THEN cw.SUBCLMN_NUM ELSE w.SUBCLMN_NUM
+        END AS [SUBCLMN_NUM]  
+   
+    , w.CLMN_DESC
 
-	, w.CLMN_DESC
-
-
-	, SUBSTRING(cw.LINE_NUM_96,1,3) as [LINE_NUM_96]		
-	, SUBSTRING(cw.CLMN_NUM_96,1,3) as [CLMN_NUM_96]
-
+	, cw.LINE_NUM_96 as [LINE_NUM_96]
+	, cw.SUBLINE_NUM_96 as [SUBLINE_NUM_96]
+			
+	, cw.CLMN_NUM_96 as [CLMN_NUM_96]
+	, cw.SUBCLMN_NUM_96 as [SUBCLMN_NUM_96]
 
 	, a.ALPHNMRC_ITM_TXT as ALPHA
 	, n.ITM_VAL_NUM as NMRC
 	, na.ALPHNMRC_ITM_TXT as NMRC_DESC
-    INTO mcrFormData
 
+    INTO mcrFormData
     FROM MCR_AVAILABLE yes
         LEFT JOIN MCR_NEW_RPT r ON
             r.FORM = yes.FORM
@@ -58,40 +70,37 @@ SELECT
                 AND w.WKSHT_CD = yes.WKSHT_CD
 
                 LEFT JOIN MCR_CROSSWALK cw ON
-                    w.FORM_NUM = cw.FORM_NUM_96	
+                    w.FORM_NUM = cw.FORM_NUM_96
                     AND w.WKSHT_CD = cw.WKSHT_CD_96
                     AND w.LINE_NUM = cw.LINE_NUM_96 
-                    AND w.CLMN_NUM = SUBSTRING(cw.CLMN_NUM_96,1,3)                   
-                    -- We have a 2010 and 1996 version of data
-                    -- The IDs don't match up.
-                    -- Normalize on the 2010 "identifiers" for 1996
-                    -- 2010 uses its own.
-
+                    AND w.CLMN_NUM = cw.CLMN_NUM_96 
+                    
+                LEFT JOIN MCR_NEW_ALPHA a ON
+                        a.CLMN_NUM != '00000' AND
+                        a.WKSHT_CD = w.WKSHT_CD
+                        AND a.FORM = w.FORM_NUM
+                        AND a.IMPORT_DT = r.IMPORT_DT
+                        AND a.RPT_REC_NUM = r.RPT_REC_NUM
+                        AND w.LINE_NUM = SUBSTRING(a.LINE_NUM,1,3)
+                        AND w.CLMN_NUM = SUBSTRING(a.CLMN_NUM,1,3)                    
+                                   
                 LEFT JOIN MCR_NEW_NMRC n ON	
                     n.WKSHT_CD = w.WKSHT_CD
-                    AND n.FORM = w.FORM_NUM		
+                    AND n.FORM = w.FORM_NUM	
                     AND n.IMPORT_DT = r.IMPORT_DT
                     AND n.RPT_REC_NUM = r.RPT_REC_NUM
                     AND w.LINE_NUM = SUBSTRING(n.LINE_NUM,1,3)
-                    AND w.CLMN_NUM = SUBSTRING(n.CLMN_NUM,1,3) 	
+                    AND w.CLMN_NUM = SUBSTRING(n.CLMN_NUM,1,3) 
 
                         LEFT JOIN MCR_NEW_ALPHA na ON
                             na.WKSHT_CD = n.WKSHT_CD
-                            AND na.FORM = n.FORM		
+                            AND na.FORM = n.FORM
                             AND na.IMPORT_DT = n.IMPORT_DT
                             AND na.RPT_REC_NUM = n.RPT_REC_NUM
                             AND na.LINE_NUM = n.LINE_NUM
                             AND na.CLMN_NUM = '00000'
 
 
-                LEFT JOIN MCR_NEW_ALPHA a ON
-                        a.CLMN_NUM != '00000'
-                        AND a.WKSHT_CD = w.WKSHT_CD	
-                        AND a.FORM = w.FORM_NUM		
-                        AND a.IMPORT_DT = r.IMPORT_DT
-                        AND a.RPT_REC_NUM = r.RPT_REC_NUM
-                        AND w.LINE_NUM = SUBSTRING(a.LINE_NUM,1,3)
-                        AND w.CLMN_NUM = SUBSTRING(a.CLMN_NUM,1,3);
 
 DROP INDEX IF EXISTS mcrFormData_a ON mcrFormData;
 CREATE INDEX mcrFormData_a ON mcrFormData (FORM, WKSHT, WKSHT_CD);
