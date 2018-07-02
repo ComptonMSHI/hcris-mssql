994bc224d1a352415ee8b6b89a9609a86d28605c
D.C. Compton <code@dccompton.com>
2018-07-02 16:07:25 -0500

    Update commandline

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-# Assumes you pass in a password or env variable with the password as the first argument.
-# ./install.sh $DB_PASS > install.log &
+# Assumes you pass in a password or env variable with the password as the third argument.
+# ./install.sh "data.informatic.ist,37136" UABMSHIMCR $DB_PASS &
 
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-crosswalk.sql > psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-alpha-load.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-nmrc-load.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-rpt-load.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-facts.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-providers.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-rows.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-availability.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-initialize.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-load.sql >> psm.log
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i install.sql > install.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-crosswalk.sql > psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-mcr-alpha-load.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-mcr-nmrc-load.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-mcr-rpt-load.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-table-facts.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-table-providers.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-table-rows.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-worksheets-availability.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-worksheets-initialize.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-worksheets-load.sql >> psm.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i install.sql > install.log
