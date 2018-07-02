ccca625a59bfe5986fcda8d2119e14e0d07b1cf5
D.C. Compton <code@dccompton.com>
2018-07-02 16:05:45 -0500

    Add install script

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 # Assumes you pass in a password or env variable with the password as the first argument.
 # ./install.sh $DB_PASS > install.log &
 
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-crosswalk.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-alpha-load.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-nmrc-load.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-rpt-load.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-facts.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-providers.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-rows.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-availability.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-initialize.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-load.sql
-sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i install.sql
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-crosswalk.sql > psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-alpha-load.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-nmrc-load.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-mcr-rpt-load.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-facts.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-providers.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-table-rows.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-availability.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-initialize.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i psm-worksheets-load.sql >> psm.log
+sqlcmd -S "data.informatic.ist,37136" -d UABMSHIMCR -U SA -P $1 -i install.sql > install.log
