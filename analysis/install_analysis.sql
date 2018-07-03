5511c993a79ccd755a3144df6efda37ac172b331
D.C. Compton <code@dccompton.com>
2018-07-02 21:18:11 -0500

    Adjust queries for analysis.

--- analysis/install_analysis.sql
+++ analysis/install_analysis.sql
@@ -12,11 +12,11 @@
 
 DECLARE @INSTALL_PRODUCTION INT = 1
 
-print '*** INSTALL: Build Form Table'
-EXEC spLoadTableForm @ProductionMode = @INSTALL_PRODUCTION;
-
 print '*** INSTALL: Build Row Table'
 EXEC spLoadTableRows @ProductionMode = @INSTALL_PRODUCTION;
+
+print '*** INSTALL: Build Form Table'
+EXEC spLoadTableForm @ProductionMode = @INSTALL_PRODUCTION;
 
 print '*** INSTALL: Build Provider Table'
 EXEC spLoadTableProviders @ProductionMode = @INSTALL_PRODUCTION;
--- analysis/psm-table-form.sql
+++ analysis/psm-table-form.sql
@@ -93,6 +93,120 @@ IF @ProductionMode = 1
 
 
 
+
+        DROP TABLE IF EXISTS mcrFormData;
+
+        print '*** LOADING ALPHA DATA'
+        SELECT
+            r.IMPORT_DT
+            , r.FORM
+            , r.PRVDR_NUM
+            , r.FY_BGN_DT
+            , r.FY_END_DT
+            , r.RPT_REC_NUM
+
+            , f.WKSHT
+            , f.WKSHT_CD
+            , f.SHEET_NAME
+            , f.SECTION_NAME
+            , f.SUBSECTION_NAME
+
+            , f.LINE_NUM
+            , f.SUBLINE_NUM    
+            , f.LINE_DESC	
+
+            , f.CLMN_NUM
+            , f.SUBCLMN_NUM 
+            , f.CLMN_DESC
+
+            , f.LINE_NUM_96
+            , f.SUBLINE_NUM_96
+                    
+            , f.CLMN_NUM_96
+            , f.SUBCLMN_NUM_96
+
+            , a.ALPHA as ALPHA
+
+        INTO mcrFormData
+        FROM MCR_NEW_RPT r
+
+            LEFT JOIN mcrFormData_Alpha a ON
+                a.RPT_REC_NUM = r.RPT_REC_NUM
+                AND a.IMPORT_DT = r.IMPORT_DT
+
+            LEFT JOIN mcrForm f ON
+                f.FORM = a.FORM
+                AND f.WKSHT_CD = a.WKSHT_CD
+                AND f.FORM =f.FORM
+                AND f.LINE_NUM = a.LINE_NUM
+                AND f.CLMN_NUM = a.CLMN_NUM
+                AND f.SUBLINE_NUM = a.SUBLINE_NUM
+                AND f.SUBCLMN_NUM = a.SUBCLMN_NUM;        
+
+
+        print '*** LOADING NMRC DATA'
+        SELECT
+            r.IMPORT_DT
+            , r.FORM
+            , r.PRVDR_NUM
+            , r.FY_BGN_DT
+            , r.FY_END_DT
+            , r.RPT_REC_NUM
+
+            , f.WKSHT
+            , f.WKSHT_CD
+            , f.SHEET_NAME
+            , f.SECTION_NAME
+            , f.SUBSECTION_NAME
+
+            , f.LINE_NUM
+            , f.SUBLINE_NUM    
+            , f.LINE_DESC	
+
+            , f.CLMN_NUM
+            , f.SUBCLMN_NUM 
+            , f.CLMN_DESC
+
+            , f.LINE_NUM_96
+            , f.SUBLINE_NUM_96
+                    
+            , f.CLMN_NUM_96
+            , f.SUBCLMN_NUM_96
+
+            , n.NMRC as NMRC
+            , na.ALPHA as NMRC_DESC
+
+            INTO mcrFormData
+            FROM MCR_NEW_RPT r 
+            
+                LEFT JOIN mcrFormData_Nmrc n ON	
+                    n.RPT_REC_NUM = r.RPT_REC_NUM
+                    AND n.IMPORT_DT = r.IMPORT_DT
+                        LEFT JOIN mcrFormData_Alpha_Desc na ON
+                            na.WKSHT_CD = n.WKSHT_CD
+                            AND na.FORM = n.FORM
+                            AND na.IMPORT_DT = n.IMPORT_DT
+                            AND na.RPT_REC_NUM = n.RPT_REC_NUM
+                            AND na.LINE_NUM = n.LINE_NUM            
+
+                INNER JOIN mcrForm f ON
+                    f.FORM = n.FORM
+                    AND f.WKSHT_CD = n.WKSHT_CD
+                    AND f.LINE_NUM = n.LINE_NUM
+                    AND f.CLMN_NUM = n.CLMN_NUM
+                    AND f.SUBLINE_NUM = n.SUBLINE_NUM
+                    AND f.SUBCLMN_NUM = n.SUBCLMN_NUM;        
+                    
+
+
+
+            DROP INDEX IF EXISTS mcrFormData_a ON mcrFormData;
+            CREATE INDEX mcrFormData_a ON mcrFormData (FORM ASC, WKSHT_CD ASC, LINE_NUM ASC, SUBLINE_NUM ASC, CLMN_NUM ASC, SUBCLMN_NUM ASC); 
+
+            DROP INDEX IF EXISTS mcrFormData_b ON mcrFormData;
+            CREATE INDEX mcrFormData_e ON mcrFormData (PRVDR_NUM ASC, FY_BGN_DT ASC, FY_END_DT ASC, RPT_REC_NUM ASC);
+
+
             -- END PRODUCTION MODE
             END
         ELSE
