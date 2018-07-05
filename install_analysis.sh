a54c494670f4c1ccd193b6e4a7582cddea2ab2c8
D.C. Compton <code@dccompton.com>
2018-07-05 07:12:39 -0500

    Correct linux shell script errors.

--- install_analysis.sh
+++ install_analysis.sh
@@ -1,8 +1,8 @@
 # Assumes you pass in a password or env variable with the password as the third argument.
 # ./install_analysis.sh "data.informatic.ist,37136" UABMSHIMCR $DB_PASS &
 
-sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-table-facts.sql -o install_analysis.log
-sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-table-providers.sql -o install_analysis.log
-sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-table-rows.sql -o install_analysis.log
-sqlcmd -S $1 -d $2 -U SA -P $3 -i psm-table-form.sql -o install_analysis.log
-sqlcmd -S $1 -d $2 -U SA -P $3 -i install.sql -o install_analysis.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i analysis/psm-table-facts.sql -o install_analysis.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i analysis/psm-table-providers.sql -o install_analysis.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i analysis/psm-table-rows.sql -o install_analysis.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i analysis/psm-table-form.sql -o install_analysis.log
+sqlcmd -S $1 -d $2 -U SA -P $3 -i analysis/install_analysis.sql -o install_analysis.log
