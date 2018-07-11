548a3e79618c556bb21f87154c9a2081297a1fb4
D.C. Compton <code@dccompton.com>
2018-07-10 22:16:47 -0500

    Alter approach to building formdata table.  Add FORM_EQUIV field

--- analysis/psm-table-form.sql
+++ analysis/psm-table-form.sql
@@ -104,10 +104,45 @@ IF @ProductionMode = 1
 
         print '*** LOADING ALPHA DATA';
 
-        WITH CombinedData AS (
+        CREATE TABLE mcrFormData (
+            IMPORT_DT datetime NULL,
+            FORM char(10) NULL,
+            FORM_EQUIV char(10) NULL,
+            PRVDR_NUM char(6) NULL,
+            FY_BGN_DT datetime NULL,
+            FY_END_DT datetime NULL,
+            RPT_REC_NUM float(8) NOT NULL,
+            WKSHT varchar(3) NULL,
+            WKSHT_CD varchar(7) NULL,
+            SHEET_NAME varchar(500) NULL,
+            SECTION_NAME varchar(500) NULL,
+            SUBSECTION_NAME varchar(500) NULL,
+            LINE_NUM varchar(6) NULL,
+            SUBLINE_NUM varchar(2) NULL,
+            LINE_DESC varchar(500) NULL,
+            CLMN_NUM varchar(6) NULL,
+            SUBCLMN_NUM varchar(2) NULL,
+            CLMN_DESC varchar(500) NULL,
+            WKSHT_CD_96 char(7) NULL,
+            LINE_NUM_96 varchar(6) NULL,
+            SUBLINE_NUM_96 varchar(2) NULL,
+            CLMN_NUM_96 varchar(6) NULL,
+            SUBCLMN_NUM_96 varchar(2) NULL,
+            NMRC float(8) NULL,
+            NMRC_DESC varchar(255) NULL,
+            COSTCODE varchar(20) NULL,
+            ALPHA varchar(255) NULL
+        );
+
+-- 2552-96 CROSSWALKED ALPHA
+    INSERT INTO mcrFormData
         SELECT
             r.IMPORT_DT
             , r.FORM
+            , CASE 
+                WHEN f.WKSHT_CD_96 Is Not Null OR r.FORM = '2552-10' 
+                    THEN '2552-10' ELSE '2552-96'
+                END AS [FORM_EQUIV]
             , r.PRVDR_NUM
             , r.FY_BGN_DT
             , r.FY_END_DT
@@ -140,32 +175,91 @@ IF @ProductionMode = 1
             , NULL as COSTCODE
             , a.ALPHA as ALPHA
 
-        
-        FROM MCR_NEW_RPT r
+        FROM mcrForm f
 
-            LEFT JOIN mcrFormData_Alpha a ON
-                a.RPT_REC_NUM = r.RPT_REC_NUM
-                AND a.IMPORT_DT = r.IMPORT_DT
-
-            LEFT JOIN mcrForm f ON
+            INNER JOIN mcrFormData_Alpha a ON
                 f.FORM = a.FORM
-                    AND 
-                    ((f.WKSHT_CD_96 = a.WKSHT_CD
-                    AND f.LINE_NUM_96 = a.LINE_NUM
-                    AND f.CLMN_NUM_96 = a.CLMN_NUM
-                    AND f.SUBLINE_NUM_96 = a.SUBLINE_NUM
-                    AND f.SUBCLMN_NUM_96 = a.SUBCLMN_NUM) OR
-                    (f.WKSHT_CD = a.WKSHT_CD
-                    AND f.LINE_NUM = a.LINE_NUM
-                    AND f.CLMN_NUM = a.CLMN_NUM
-                    AND f.SUBLINE_NUM = a.SUBLINE_NUM
-                    AND f.SUBCLMN_NUM = a.SUBCLMN_NUM))         
+                AND f.WKSHT_CD_96 = a.WKSHT_CD
+                AND f.LINE_NUM_96 = a.LINE_NUM
+                AND f.CLMN_NUM_96 = a.CLMN_NUM
+                AND f.SUBLINE_NUM_96 = a.SUBLINE_NUM
+                AND f.SUBCLMN_NUM_96 = a.SUBCLMN_NUM
+                
+            INNER JOIN MCR_NEW_RPT r ON
+                a.RPT_REC_NUM = r.RPT_REC_NUM
+                AND a.IMPORT_DT = r.IMPORT_DT 
+                
+        WHERE f.WKSHT_CD_96 Is Not Null AND a.FORM = '2552-96';        
 
-        UNION
 
+-- 2552-10 and 2552-96 NON-CROSSWALKED ALPHA
+    INSERT INTO mcrFormData
         SELECT
             r.IMPORT_DT
             , r.FORM
+            , CASE 
+                WHEN f.WKSHT_CD_96 Is Not Null OR r.FORM = '2552-10' 
+                    THEN '2552-10' ELSE '2552-96'
+                END AS [FORM_EQUIV]
+            , r.PRVDR_NUM
+            , r.FY_BGN_DT
+            , r.FY_END_DT
+            , r.RPT_REC_NUM
+
+            , f.WKSHT
+            , f.WKSHT_CD
+            , f.SHEET_NAME
+            , f.SECTION_NAME
+            , f.SUBSECTION_NAME
+
+            , f.LINE_NUM
+            , f.SUBLINE_NUM    
+            , f.LINE_DESC	
+
+            , f.CLMN_NUM
+            , f.SUBCLMN_NUM 
+            , f.CLMN_DESC
+
+            , f.WKSHT_CD_96
+
+            , f.LINE_NUM_96
+            , f.SUBLINE_NUM_96
+                    
+            , f.CLMN_NUM_96
+            , f.SUBCLMN_NUM_96
+
+            , NULL as NMRC
+            , NULL as NMRC_DESC
+            , NULL as COSTCODE
+            , a.ALPHA as ALPHA
+
+        FROM mcrForm f
+
+            INNER JOIN mcrFormData_Alpha a ON
+                f.FORM = a.FORM
+                AND f.WKSHT_CD = a.WKSHT_CD
+                AND f.LINE_NUM = a.LINE_NUM
+                AND f.CLMN_NUM = a.CLMN_NUM
+                AND f.SUBLINE_NUM = a.SUBLINE_NUM
+                AND f.SUBCLMN_NUM = a.SUBCLMN_NUM
+                
+            INNER JOIN MCR_NEW_RPT r ON
+                a.RPT_REC_NUM = r.RPT_REC_NUM
+                AND a.IMPORT_DT = r.IMPORT_DT 
+
+        WHERE f.WKSHT_CD_96 Is Null;           
+
+print '*** LOADING NMRC DATA';
+
+-- 2552-96 CROSSWALKED NMRC
+    INSERT INTO mcrFormData
+        SELECT
+            r.IMPORT_DT
+            , r.FORM
+            , CASE 
+                WHEN f.WKSHT_CD_96 Is Not Null OR r.FORM = '2552-10' 
+                    THEN '2552-10' ELSE '2552-96'
+                END AS [FORM_EQUIV]
             , r.PRVDR_NUM
             , r.FY_BGN_DT
             , r.FY_END_DT
@@ -198,9 +292,17 @@ IF @ProductionMode = 1
             , na.COSTCODE as COSTCODE
             , NULL as ALPHA
 
-            FROM MCR_NEW_RPT r 
-            
-                LEFT JOIN mcrFormData_Nmrc n ON	
+            FROM mcrForm f        
+
+                INNER JOIN mcrFormData_Nmrc n ON
+                f.FORM = n.FORM
+                    AND f.WKSHT_CD_96 = n.WKSHT_CD
+                    AND f.LINE_NUM_96 = n.LINE_NUM
+                    AND f.CLMN_NUM_96 = n.CLMN_NUM
+                    AND f.SUBLINE_NUM_96 = n.SUBLINE_NUM
+                    AND f.SUBCLMN_NUM_96 = n.SUBCLMN_NUM     
+
+                INNER JOIN MCR_NEW_RPT r ON	
                     n.RPT_REC_NUM = r.RPT_REC_NUM
                     AND n.IMPORT_DT = r.IMPORT_DT
                         LEFT JOIN mcrFormData_Alpha_Desc na ON
@@ -208,25 +310,76 @@ IF @ProductionMode = 1
                             AND na.FORM = n.FORM
                             AND na.IMPORT_DT = n.IMPORT_DT
                             AND na.RPT_REC_NUM = n.RPT_REC_NUM
-                            AND na.LINE_NUM = n.LINE_NUM            
+                            AND na.LINE_NUM = n.LINE_NUM
 
-                INNER JOIN mcrForm f ON
+            WHERE f.WKSHT_CD_96 Is Not Null AND n.FORM = '2552-96';   
+
+-- 2552-10 AND 2552-96 NON-CROSSWALKED NMRC
+        INSERT INTO mcrFormData
+        SELECT
+            r.IMPORT_DT
+            , r.FORM
+            , CASE 
+                WHEN f.WKSHT_CD_96 Is Not Null OR r.FORM = '2552-10' 
+                    THEN '2552-10' ELSE '2552-96'
+                END AS [FORM_EQUIV]
+            , r.PRVDR_NUM
+            , r.FY_BGN_DT
+            , r.FY_END_DT
+            , r.RPT_REC_NUM
+
+            , f.WKSHT
+            , f.WKSHT_CD
+            , f.SHEET_NAME
+            , f.SECTION_NAME
+            , f.SUBSECTION_NAME
+
+            , f.LINE_NUM
+            , f.SUBLINE_NUM    
+            , f.LINE_DESC	
+
+            , f.CLMN_NUM
+            , f.SUBCLMN_NUM 
+            , f.CLMN_DESC
+
+            , f.WKSHT_CD_96
+
+            , f.LINE_NUM_96
+            , f.SUBLINE_NUM_96
+                    
+            , f.CLMN_NUM_96
+            , f.SUBCLMN_NUM_96
+
+            , n.NMRC as NMRC
+            , na.ALPHA as NMRC_DESC
+            , na.COSTCODE as COSTCODE
+            , NULL as ALPHA
+
+            
+            FROM mcrForm f        
+
+                INNER JOIN mcrFormData_Nmrc n ON
                 f.FORM = n.FORM
-                    AND 
-                    ((f.WKSHT_CD_96 = n.WKSHT_CD
-                    AND f.LINE_NUM_96 = n.LINE_NUM
-                    AND f.CLMN_NUM_96 = n.CLMN_NUM
-                    AND f.SUBLINE_NUM_96 = n.SUBLINE_NUM
-                    AND f.SUBCLMN_NUM_96 = n.SUBCLMN_NUM) OR
-                    (f.WKSHT_CD = n.WKSHT_CD
+                    AND f.WKSHT_CD = n.WKSHT_CD
                     AND f.LINE_NUM = n.LINE_NUM
                     AND f.CLMN_NUM = n.CLMN_NUM
                     AND f.SUBLINE_NUM = n.SUBLINE_NUM
-                    AND f.SUBCLMN_NUM = n.SUBCLMN_NUM))        
-            ) 
-            SELECT *
-            INTO mcrFormData
-            FROM CombinedData;       
+                    AND f.SUBCLMN_NUM = n.SUBCLMN_NUM     
+
+                INNER JOIN MCR_NEW_RPT r ON	
+                    n.RPT_REC_NUM = r.RPT_REC_NUM
+                    AND n.IMPORT_DT = r.IMPORT_DT
+                        LEFT JOIN mcrFormData_Alpha_Desc na ON
+                            na.WKSHT_CD = n.WKSHT_CD
+                            AND na.FORM = n.FORM
+                            AND na.IMPORT_DT = n.IMPORT_DT
+                            AND na.RPT_REC_NUM = n.RPT_REC_NUM
+                            AND na.LINE_NUM = n.LINE_NUM
+                
+            WHERE f.WKSHT_CD_96 Is Null; 
+
+
+
 
 
 
